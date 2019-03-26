e7e75bfa97298151cdca1f546892135ea2d71064
Oga-Emma <sevenapps2018@gmail.com>
2019-03-27 00:08:34 +0100

    From 7

--- lib/homeWidget.dart
+++ lib/homeWidget.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
-import 'package:carousel_pro/carousel_pro.dart';
 import 'preview_coursePage.dart';
 import 'package:transparent_image/transparent_image.dart';
+import 'package:carousel_pro/carousel_pro.dart';
 
-
-class homeWget extends StatelessWidget{
+class homeWget extends StatelessWidget {
   static FirebaseAnalytics analytics = FirebaseAnalytics();
-  
- @override
- Widget build(BuildContext context) {
-   
+
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Home',
       theme: ThemeData(
@@ -18,24 +16,22 @@ class homeWget extends StatelessWidget{
       ),
       home: home_page(),
     );
- }
-
+  }
 }
 
-class home_page extends StatefulWidget{
+class home_page extends StatefulWidget {
   home_page({Key key}) : super(key: key);
-  
-@override
+
+  @override
   _main createState() => _main();
 }
 
-class _main extends State<home_page>{
+class _main extends State<home_page> {
+  final List<Todo> todos = new List(0);
+  final Color pageBackgroudcolor = Color.fromRGBO(63, 63, 63, 1);
+  final Color pageContainerColor = Color.fromRGBO(99, 98, 98, 1);
 
-final List<Todo> todos = new List(0);
-final Color pageBackgroudcolor = Color.fromRGBO(63, 63, 63, 1);
-final Color pageContainerColor = Color.fromRGBO(99, 98, 98, 1);
-
-Card getStructuredGridCell(name, image) {
+  Card getStructuredGridCell(name, image) {
     return new Card(
         elevation: 1.5,
         child: new Column(
@@ -43,7 +39,7 @@ Card getStructuredGridCell(name, image) {
           mainAxisSize: MainAxisSize.min,
           verticalDirection: VerticalDirection.down,
           children: <Widget>[
-            new Image(image: new AssetImage('https://firebasestorage.googleapis.com/v0/b/liliteach-3e0c2.appspot.com/o/1170x500-01-1.jpg?alt=media&token=664522c6-1a23-48c4-baa8-82b59c2884df')),
+            new Image(image: new AssetImage(image)),
             new Center(
               child: new Text(name),
             )
@@ -51,70 +47,60 @@ Card getStructuredGridCell(name, image) {
         ));
   }
 
- @override
- Widget build(BuildContext context) {
-   return ListView(
-    
-     children: <Widget>[
-        ListView(
-     shrinkWrap: true,
-     children: <Widget>[
-      Container(
-      child: new SizedBox(
-            height: 250.0,
-            width: double.infinity,
-            child: new Carousel(
-              images: [
-                new NetworkImage('https://firebasestorage.googleapis.com/v0/b/liliteach-3e0c2.appspot.com/o/1170x500-01-1.jpg?alt=media&token=664522c6-1a23-48c4-baa8-82b59c2884df'),
-                new NetworkImage('https://firebasestorage.googleapis.com/v0/b/liliteach-3e0c2.appspot.com/o/kids-banner.jpg?alt=media&token=9856e43a-0c4a-4283-8852-e19eab877363'),
-                new NetworkImage('https://firebasestorage.googleapis.com/v0/b/liliteach-3e0c2.appspot.com/o/LENOIR-4.jpg?alt=media&token=a335a96f-28b8-4b0c-ac91-54707d80eed2'),
-                new NetworkImage('https://firebasestorage.googleapis.com/v0/b/liliteach-3e0c2.appspot.com/o/wgep067_thumbRAISEYOURHAND.png?alt=media&token=aba74d31-845c-44c1-b202-e7a711519119')
-              ],
-            )
-        ),
-    ),
-   
-       Container(
-         color: pageBackgroudcolor,
-         child: Padding(
-padding: const EdgeInsets.all(3.0),
-child: Center(   child: Card(  
-   child: InkWell(
-     onTap: null,
-     child: SizedBox(
-       height: 150,
-       width: double.infinity,
-       child: FadeInImage.memoryNetwork(
-  placeholder: kTransparentImage,
-  image:'https://firebasestorage.googleapis.com/v0/b/liliteach-3e0c2.appspot.com/o/course%20pic1.jpg?alt=media&token=aaaa1ebe-1f34-4d2a-a475-7de97ed34fbf',
-            fit: BoxFit.cover,
-            ),
-  
-     ) ) ), ), 
-    ),),
-
-
-GridView.count(
-      primary: true,
-      padding: const EdgeInsets.all(1.0),
-      crossAxisCount: 1,
-      childAspectRatio: 0.85,
-      mainAxisSpacing: 1.0,
-      crossAxisSpacing: 1.0,
+  @override
+  Widget build(BuildContext context) {
+    return Column(
       children: <Widget>[
-        getStructuredGridCell("Facebook", "social/facebook.png"),
-        getStructuredGridCell("Twitter", "social/twitter.png"),
-        getStructuredGridCell("Instagram", "social/instagram.png"),
-        getStructuredGridCell("Linkedin", "social/linkedin.png"),
-        getStructuredGridCell("Gooogle Plus", "social/google_plus.png"),
-        getStructuredGridCell("Launcher Icon", "ic_launcher.png"),
+        Container(
+          height: 250.0,
+          color: Colors.red,
+          child: Carousel(
+            images: [
+              NetworkImage(
+                  'https://firebasestorage.googleapis.com/v0/b/liliteach-3e0c2.appspot.com/o/1170x500-01-1.jpg?alt=media&token=664522c6-1a23-48c4-baa8-82b59c2884df'),
+              NetworkImage(
+                  'https://firebasestorage.googleapis.com/v0/b/liliteach-3e0c2.appspot.com/o/kids-banner.jpg?alt=media&token=9856e43a-0c4a-4283-8852-e19eab877363'),
+              NetworkImage(
+                  'https://firebasestorage.googleapis.com/v0/b/liliteach-3e0c2.appspot.com/o/LENOIR-4.jpg?alt=media&token=a335a96f-28b8-4b0c-ac91-54707d80eed2'),
+              NetworkImage(
+                  'https://firebasestorage.googleapis.com/v0/b/liliteach-3e0c2.appspot.com/o/wgep067_thumbRAISEYOURHAND.png?alt=media&token=aba74d31-845c-44c1-b202-e7a711519119')
+            ],
+          ),
+        ),
+        Container(
+          height: 150,
+          width: double.infinity,
+          color: pageBackgroudcolor,
+          padding: const EdgeInsets.all(2.0),
+          child: FadeInImage.memoryNetwork(
+            placeholder: kTransparentImage,
+            image:
+                'https://firebasestorage.googleapis.com/v0/b/liliteach-3e0c2.appspot.com/o/course%20pic1.jpg?alt=media&token=aaaa1ebe-1f34-4d2a-a475-7de97ed34fbf',
+            fit: BoxFit.cover,
+          ),
+        ),
+        Flexible(
+          child: Container(
+            color: Colors.green,
+            child: GridView.count(
+              primary: true,
+              padding: const EdgeInsets.all(1.0),
+              crossAxisCount: 2,
+              childAspectRatio: 1.1,
+              mainAxisSpacing: 1.0,
+              crossAxisSpacing: 1.0,
+              children: <Widget>[
+                getStructuredGridCell("Facebook", "social/facebook.png"),
+                getStructuredGridCell("Twitter", "social/twitter.png"),
+                getStructuredGridCell("Instagram", "social/instagram.png"),
+                getStructuredGridCell("Linkedin", "social/linkedin.png"),
+                getStructuredGridCell("Gooogle Plus", "social/google_plus.png"),
+                getStructuredGridCell("Launcher Icon", "ic_launcher.png"),
+              ],
+            ),
+          ),
+        ),
       ],
-    ),
-
-
-    ],
-   )
-,
-     ],
-   );}
+    );
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -30,54 +30,66 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class ContainerWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blueAccent,
+        child: SampleAppPage(),
+      ),
+    );
+  }
+}
+
 class SampleAppPage extends StatefulWidget {
   SampleAppPage({Key key}) : super(key: key);
-    @override
+  @override
   _SampleAppPageState createState() => _SampleAppPageState();
 }
 
 class _SampleAppPageState extends State<SampleAppPage> {
- 
   int _currentIndex = 0;
   final List<Widget> _children = [
     homeWget(),
     searchWidget(),
     saveWideget(),
     profileWideget()
-     ];
+  ];
+
   void onTabTapped(int index) {
-   setState(() {
-     _currentIndex = index;
-   });
- }
+    setState(() {
+      _currentIndex = index;
+    });
+  }
 
   Widget build(BuildContext context) {
-   return Scaffold(
-   
-     body: _children[_currentIndex], 
-     bottomNavigationBar: BottomNavigationBar(
-       
-       onTap: onTabTapped, // new
-       currentIndex: _currentIndex, // new       
-       type: BottomNavigationBarType.fixed,
-       items: [
-         BottomNavigationBarItem(
-           icon: new Icon(Icons.home),
-           title: new Text('Home'),
-         ),
-         BottomNavigationBarItem(
-           icon: new Icon(Icons.search),
-           title: new Text('Search'),
-         ),BottomNavigationBarItem(
-           icon: new Icon(Icons.save),
-           title: new Text('Saved'),
-         ),
-         BottomNavigationBarItem(
-           icon: Icon(Icons.person),
-           title: new Text('Profile')
-         )
-       ],
-     ),
-   );
- }
+    return Scaffold(
+      body: Container(
+        child: _children[_currentIndex],
+//        color: Colors.blueAccent,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTabTapped, // new
+        currentIndex: _currentIndex, // new
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.home),
+            title: new Text('Home'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.search),
+            title: new Text('Search'),
+          ),
+          BottomNavigationBarItem(
+            icon: new Icon(Icons.save),
+            title: new Text('Saved'),
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person), title: new Text('Profile'))
+        ],
+      ),
+    );
+  }
 }
