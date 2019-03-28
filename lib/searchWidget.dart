f0031b7722245ac68ff92b8ca375e07db2e16423
Lambert Nnadi <nnadi406@gmail.com>
2019-03-28 04:20:54 +0000

    new commit

--- lib/searchWidget.dart
+++ lib/searchWidget.dart
@@ -24,14 +24,30 @@ class _Search_page extends StatefulWidget{
 }
 
 class _main extends State<_Search_page>{
-
+final Color pageBackgroudcolor = Color.fromRGBO(63, 63, 63, 1);
+  final Color pageContainerColor = Color.fromRGBO(100, 99, 99, 1);
  @override
  Widget build(BuildContext context) {
    return Scaffold (
-     appBar: AppBar(
-       title: Text('Search'),
-     ),
-     body: Center(child: Text('Search')),
+     backgroundColor: pageBackgroudcolor,
+     body: Column(
+       children: <Widget>[
+         Container(
+           child: TextField(
+  decoration: new InputDecoration(
+      border: new OutlineInputBorder(
+        borderRadius: const BorderRadius.all(
+          const Radius.circular(10.0),
+        ),
+      ),
+      filled: true,
+      hintStyle: new TextStyle(color: Colors.grey[800]),
+      hintText: "Type in your text",
+      fillColor: Colors.white70),
+),
+         )
+       ],
+     )
    );
  }
 }
